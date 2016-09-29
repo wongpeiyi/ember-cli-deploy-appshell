f9a3faf7d91224653097d6f32b1f3ea8ee003b62
Wong Pei Yi <wongpeiyi@gmail.com>
2016-09-30 01:21:44 +0800

    Clean up index

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 /* jshint node: true */
 "use strict";
-let path = require('path');
-let uglify = require('uglify-js');
+
 let DeployPluginBase = require('ember-cli-deploy-plugin');
 let fs = require('fs');
+let path = require('path');
 let minimatch = require('minimatch');
+let uglify = require('uglify-js');
 
 module.exports = {
   name: 'ember-cli-deploy-appshell',
@@ -17,80 +18,88 @@ module.exports = {
     if (type === 'head') {
       this.rootURL = config.rootURL;
       this.modulePrefix = config.modulePrefix;
-      return `<script src="${this.rootURL}bootloader.js"></script>`;
+      return `<script src="${config.rootURL}bootloader.js"></script>`;
     }
   },
 
-  createDeployPlugin: function(options) {
+  createDeployPlugin(options) {
     let rootURL = () => this.rootURL;
     let modulePrefix = () => this.modulePrefix;
+
     let DeployPlugin = DeployPluginBase.extend({
       name: options.name,
+
       defaultConfig: {
         excludePattern: '{robots.txt,crossdomain.xml}',
         externalDependencies: [],
         prefixDomains: {},
-        prepareManifest: true,
-        distDir: function(context) {
+        buildManifest: true,
+        distDir(context) {
           return context.distDir;
         },
-        distFiles: function(context) {
+        distFiles(context) {
           return context.distFiles;
         },
       },
-      didBuild: function(context) {
-        this.prepareBootLoader(context);
-        if (this.readConfig('prepareManifest')) {
-          this.prepareManifest(context);
+
+      didBuild(context) {
+        this.buildBootLoader(context);
+        if (this.readConfig('buildManifest')) {
+          this.buildManifest(context);
         }
       },
-      prepareBootLoader: function(context) {
+
+      buildBootLoader(context) {
         let distDir = this.readConfig('distDir');
-        fs.writeFileSync(path.join(distDir, 'bootloader.js'), this.renderBootloader(modulePrefix()));
+        fs.writeFileSync(path.join(distDir, 'bootloader.js'), this.writeBootloader(modulePrefix()));
         if (context.distFiles) {
           context.distFiles.push('bootloader.js');
         }
       },
-      prepareManifest: function(context) {
+
+      buildManifest(context) {
         let distDir = this.readConfig('distDir');
         let files = this.readConfig('distFiles');
-        fs.writeFileSync(path.join(distDir, 'manifest.appcache'), this.renderManifest(files));
         let indexHTML = fs.readFileSync(path.join(distDir, 'index.html'), 'utf8');
+        fs.writeFileSync(path.join(distDir, 'manifest.appcache'), this.writeManifest(files));
         fs.writeFileSync(path.join(distDir, 'appshell.html'), indexHTML);
-        fs.writeFileSync(path.join(distDir, 'index.html'), indexHTML.replace(/<html>/i, `<html manifest=${rootURL()}manifest.appcache>`));
+        fs.writeFileSync(path.join(distDir, 'index.html'), indexHTML.replace(/<html/i, `<html manifest="${rootURL()}manifest.appcache"`));
         if (context.distFiles) {
           context.distFiles.push('manifest.appcache', 'appshell.html');
         }
       },
-      renderManifest: function(paths) {
+
+      writeManifest(paths) {
         let excludePattern = this.readConfig('excludePattern');
         let prefixDomains = this.readConfig('prefixDomains');
-        let outputPaths = paths.filter(function(p){ return !minimatch(p, excludePattern); })
-            .map(function(p) {
-              let domain = Object.keys(prefixDomains).find(function(domain) {
-                return minimatch(p, prefixDomains[domain]);
-              });
-              if (domain) {
-                return domain + p;
-              } else {
-                return p;
-              }
-            });
-        outputPaths = outputPaths.concat(this.readConfig('externalDependencies'));
+        let outputPaths = paths.filter((p) => !minimatch(p, excludePattern))
+          .map((p) => {
+            let domain = Object.keys(prefixDomains).find((domain) => minimatch(p, prefixDomains[domain]));
+            if (domain) {
+              return domain + p;
+            } else {
+              return p;
+            }
+          });
 
-        return `CACHE MANIFEST
-# ${new Date()}
-${ outputPaths.join("\n") }
-NETWORK:
-*`;
+        return [
+          'CACHE MANIFEST',
+          `# ${new Date()}`,
+        ].concat(
+          outputPaths,
+          this.readConfig('externalDependencies'),
+          'NETWORK:',
+          '*'
+        ).join("\n");
       },
-      renderBootloader: function(modulePrefix) {
+
+      writeBootloader(modulePrefix) {
         let loader = fs.readFileSync(require.resolve('loader.js'), 'utf8');
         let src = fs.readFileSync(path.join(__dirname, 'lib', 'bootloader.js'), 'utf8').replace(/MODULE_PREFIX/g, modulePrefix);
         return uglify.minify(loader + src, { fromString: true, mangle: true, compress: true }).code;
       }
     });
+
     return new DeployPlugin();
   }
-
 };
